a546c678bfb47a64abccc19591791766fc9d3433
aledelmo <delmonte.ale92@gmail.com>
2018-09-12 09:11:29 +0200

    Updated pipeline

--- processing_vs/pipeline.py
+++ processing_vs/pipeline.py
@@ -2,10 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from processing_vs.logic_vs import *
+import nibabel as nib
+import numpy as np
 
 
 def proc(volume, seeds, vessel_type, output, from_plugin):
-    data, affine, _ = load_nii(volume)
+    data, affine, vox_dim = load_nii(volume)
     points = load_pkl(seeds)
 
-    skeleton = Tree(points, affine)
+    hierarchy = Tree(points, affine, vox_dim)
+
+    if vessel_type.lower() == 'artery':
+        label_number = 17
+    else:
+        label_number = 16
+
+    # output_data = hierarchy.get_label_mask(data, label_number)
+    output_data = hierarchy.get_patches(data)
+
+    # img = nib.Nifti1Image(output_data.astype(np.int16), affine)
+    # nib.save(img, output)
