dd738309c07df01e5c54fe373f82e0871baed2fe
aledelmo <delmonte.ale92@gmail.com>
2018-09-18 09:07:25 +0200

    Implemented pipeline manager

--- processing_vs/pipeline.py
+++ processing_vs/pipeline.py
@@ -2,23 +2,33 @@
 # -*- coding: utf-8 -*-
 
 from processing_vs.logic_vs import *
-import nibabel as nib
-import numpy as np
+import os.path
 
 
-def proc(volume, seeds, vessel_type, output, from_plugin):
-    data, affine, vox_dim = load_nii(volume)
+def proc(volume, seeds, vessel_type, output):
+    data, affine, _ = load_nii(volume)
     points = load_pkl(seeds)
-
-    hierarchy = Tree(points, affine, vox_dim)
-
     if vessel_type.lower() == 'artery':
         label_number = 17
     else:
         label_number = 16
 
-    # output_data = hierarchy.get_label_mask(data, label_number)
-    output_data = hierarchy.get_patches(data)
+    cnn = Cnn()
+    hierarchy = Tree(points, affine)
 
-    # img = nib.Nifti1Image(output_data.astype(np.int16), affine)
-    # nib.save(img, output)
+    # mask_skeleton = hierarchy.get_label_mask(data, label_number)
+    # save_nii(mask_skeleton, affine, '/Users/imag2/Desktop/skeleton-label.nii.gz')
+
+    deploy_fpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'cnn_models', 'deploy.prototxt')
+    model_fpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'cnn_models',
+                               'snapshot_iter_135000.caffemodel')
+
+    cnn.load_net(deploy_fpath, model_fpath, True)
+    out_patches = cnn.infer(hierarchy.get_patches(data))
+
+    output_data = reconstruct(out_patches, data, label_number)
+
+    output_data[output_data == 1] = 17
+    output_data[output_data == 2] = 16
+
+    save_nii(output_data, affine, output)
