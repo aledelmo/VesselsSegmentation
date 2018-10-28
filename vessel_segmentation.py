7a3a8f497c54fbb3bca4c2278c22ea1e3849a3d7
aledelmo <delmonte.ale92@gmail.com>
2018-10-28 23:24:54 +0100

    main accepting combination of arteries/veins

--- vessel_segmentation.py
+++ vessel_segmentation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from time import time
 import argparse
-import sys
 import os.path
+import sys
+from time import time
+
 import processing_vs as vs
 
 __author__ = 'Alessandro Delmonte'
@@ -14,29 +15,40 @@ __email__ = 'delmonte.ale92@gmail.com'
 def main():
     """
     Call to parser and pipeline
-    """
-    volume, seeds, vessel_type, output = setup()
 
-    vs.proc(volume, seeds, vessel_type, output)
+    :rtype: void
+    """
+    volume, arteries, veins, output = setup()
+    if arteries is not None or veins is not None:
+        vs.proc(volume, arteries, veins, output)
 
 
 def setup():
     """
     Command line parser
+
+    :rtype: list of strings
     :return: arguments
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('Input_Volume', help='Name of the input reference volume', type=check_nii)
-    parser.add_argument('Initialization', help='Pickled list of initialization points', type=check_pickle)
-    parser.add_argument('Type', help='Choose between Artery and Vein', choices=['Artery', 'artery', 'Vein', 'vein'])
-    parser.add_argument('Output_Volume', help='Name of the input reference volume', type=check_out)
+    parser.add_argument('-a', '--arteries', help='Pickled list of arteries initialization points', type=check_pickle)
+    parser.add_argument('-v', '--veins', help='Pickled list of veins initialization points', type=check_pickle)
+    parser.add_argument('Output_Volume', help='Name of the output reference volume', type=check_out)
 
     args = parser.parse_args()
 
-    return args.Input_Volume, args.Initialization, args.Type, args.Output_Volume
+    return args.Input_Volume, args.arteries, args.veins, args.Output_Volume
 
 
 def check_nii(value):
+    """
+    Verify that the file has a valid NIfTI-1 extension
+
+    :param value: command line inserted string
+    :return: file absolute path
+    :rtype: string
+    """
     if value.endswith('.nii') or value.endswith('.nii.gz') and os.path.isfile(os.path.abspath(value)):
         return os.path.abspath(value)
     else:
@@ -44,6 +56,13 @@ def check_nii(value):
 
 
 def check_out(value):
+    """
+       Verify that the file has a valid NIfTI-1 extension
+
+       :param value: command line inserted string
+       :return: file absolute path
+       :rtype: string
+       """
     if value.endswith('.nii') or value.endswith('.nii.gz'):
         return os.path.abspath(value)
     else:
