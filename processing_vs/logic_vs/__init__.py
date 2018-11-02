f873f4f7fbf9b9807df38499c8ef6bd139507063
aledelmo <delmonte.ale92@gmail.com>
2018-11-02 12:43:50 +0100

    Fixes and general improvements

--- processing_vs/logic_vs/__init__.py
+++ processing_vs/logic_vs/__init__.py
@@ -4,4 +4,4 @@
 from .input_output import load_pkl, load_nii, save_nii
 from .skeleton import Tree
 from .network import Cnn
-from .segmentation import reconstruct
+from .segmentation import reconstruct, improve_seg
