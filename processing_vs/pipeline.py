dc6a6751d947cc4eed962bad5dae7cc65c4ac091
aledelmo <delmonte.ale92@gmail.com>
2018-12-20 11:36:56 +0100

    updated to newwest caffe model

--- processing_vs/pipeline.py
+++ processing_vs/pipeline.py
@@ -25,8 +25,7 @@ def proc(volume, arteries, veins, output):
     deploy_fpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'cnn_models', 'deploy.prototxt')
     model_fpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'cnn_models',
                                '2018_12_13.caffemodel')
-    # model_fpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'cnn_models',
-    #                            'benchmark.caffemodel')
+
     output_data = []
     vein_label = 16
     art_label = 17
