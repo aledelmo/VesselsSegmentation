53983bc93ffc78536dcb5190f9efd700d12d098f
aledelmo <delmonte.ale92@gmail.com>
2021-02-23 14:26:03 +0100

    updated docs

--- training/functions/pelvis_layers/__init__.py
+++ training/functions/pelvis_layers/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from .pelvis_layers import PelvisTrainDataLayer, PelvisValDataLayer
+version https://git-lfs.github.com/spec/v1
+oid sha256:04308c175303917d39df66ad3d3d77e683990ee90feed88a9d735ecd0517ab1a
+size 115
--- training/patches/__init__.py
+++ training/patches/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from .get_patches import get_patches
+version https://git-lfs.github.com/spec/v1
+oid sha256:45b03ed5d72c27ba0ac79e2a9356ff7c9ba8cc69301847977e9304f519f774a0
+size 84
